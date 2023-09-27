f81424a44b55f3b9fa214e27b5229e38b5063b9d
Julio-Baeta1 <julbay1643@gmail.com>
2023-09-27 12:35:57 +0200

    Add: Home page view tests

--- loadshedding/loadshedding_calc/tests/test_views.py
+++ loadshedding/loadshedding_calc/tests/test_views.py
@@ -5,34 +5,21 @@ from django.urls import reverse
 
 from loadshedding_calc.models import CapeTownSlots
 
-# Must research session and unmanaged table testing 
-"""
-class DaySlotViewTest(TestCase):
+class HomePageViewTest(TestCase):
 
-    def test_day_slots_view_url_location(self):
-        response = self.client.get('/loadshedding_calc/day/')
+    def testHomeViewUrlByPath(self):
+        response = self.client.get('/', {}, True)
         self.assertEqual(response.status_code, 200)
 
-    def test_day_slots_view_url_accessible_by_name(self):
-        response = self.client.get(reverse('day-slots'))
+    def testHomeViewUrlByName(self):
+        response = self.client.get(reverse('index'), {}, True)
         self.assertEqual(response.status_code, 200)
 
-    def test_day_slots_view_uses_correct_template(self):
-        response = self.client.get(reverse('day-slots'))
+    def testHomeUsesCorrectTemplate(self):
+        response = self.client.get(reverse('index'), {}, True)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'loadshedding_calc/day.html')
-
-    def test_day_slots_view_session(self):
-        # Issue a GET request.
-        self.client.get('/loadshedding_calc/day/')
-        session = self.client.session
-        session['c_day'] = 1
-        session['c_area'] = 1
-        session['c_stage'] = 1
-        response = self.client.get(reverse('day-slots'))
-        self.assertEqual(response.status_code, 200)
-"""
-
-
+        self.assertTemplateUsed(response, 'index.html')
+        self.assertTemplateUsed(response, 'base_generic.html')
+        
 ##################################################################################################
 
